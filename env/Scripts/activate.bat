a7d29f7058f45de30090a135db98c9dd4eba31e2
stalynvera <stalyn01062003@gmail.com>
2025-07-06 21:34:22 -0500

    .

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\veras\Documents\GitHub\sullyta\env"
+set "VIRTUAL_ENV=C:\Users\veras\Documents\GitHub\Pag_sully\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
